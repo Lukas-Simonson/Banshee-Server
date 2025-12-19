68a8bcb2bcdfccd0091e1516b71d1083e13d9b01
Lukas Simonson <lukas@hydradesignllc.com>
2025-12-18 17:14:59 -0700

    Adds GET /api/podcasts/feeds endpoint
    Adds the GET /api/podcasts/feeds endpoint.
    
    This endpoint allows admin users to see what feeds are currently active
    in their library.

--- Sources/Banshee-Server/Controllers/PodcastsController.swift
+++ Sources/Banshee-Server/Controllers/PodcastsController.swift
@@ -3,28 +3,12 @@ import Vapor
 
 struct PodcastsController: RouteCollection {
     func boot(routes: any RoutesBuilder) throws {
-        routes.grouped("podcasts").group(UserAuthenticator()) { podcasts in
+        try routes.grouped("podcasts").group(UserAuthenticator()) { podcasts in
             podcasts.get(use: getAllPodcasts)
             podcasts.get(":podcastID", use: getPodcast)
-
-            podcasts.grouped("feeds").group(AdminAuthMiddleware()) { feeds in
-                feeds.post(use: registerFeed)
-            }
+            
+            try podcasts.register(collection: RSSFeedController())
         }
-    }
-
-    private func registerFeed(req: Request) async throws -> PodcastDTO {
-        let feedRequest = try req.content.decode(CreatePodcastFeedRequest.self)
-
-        let rssResponse = try await req.client.get(URI(from: feedRequest.url))
-            .content.decode(RSS.self)
-
-        // Save Podcast
-        let podcast = rssResponse.channel.toModel(rssFeedURL: feedRequest.url)
-        try await podcast.save(on: req.db)
-        try await podcast.$episodes.create(rssResponse.channel.item.map { $0.toModel() }, on: req.db)
-
-        return try PodcastDTO(from: podcast)
     }
 
     private func getAllPodcasts(req: Request) async throws -> [PodcastDTO] {
@@ -57,11 +41,4 @@ extension PodcastsController {
         static var unknownID: Abort { Abort(.badRequest, reason: "Unknown podcast id provided") }
         static var invalidIDFormat: Abort { Abort(.badRequest, reason: "Invalid podcast id format") }
     }
-}
-
-// MARK: - Request Objects
-extension PodcastsController {
-    struct CreatePodcastFeedRequest: Content {
-        let url: URL
-    } 
 }
