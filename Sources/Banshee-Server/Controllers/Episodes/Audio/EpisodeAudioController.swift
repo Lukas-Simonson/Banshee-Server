e46c7d32330b806fb71be9e28a5eab60f2bed7df
Lukas Simonson <lukas@hydradesignllc.com>
2025-12-22 14:17:31 -0700

    Adds Stream Redirect
    Adds a redirect for the episode stream endpoint when no file is
    downloaded for the episode.
    
    This will allow users to listen to the podcast, regardless on if it is
    downloaded locally!

--- Sources/Banshee-Server/Controllers/Episodes/Audio/EpisodeAudioController.swift
+++ Sources/Banshee-Server/Controllers/Episodes/Audio/EpisodeAudioController.swift
@@ -17,8 +17,15 @@ struct EpisodeAudioController: RouteCollection {
             .first()
             .unwrap(or: Errors.unknownID)
 
+        if let remoteURL = audio.remoteURL, audio.localURL == nil {
+            // Redirect to podcast server, as file is not loaded.
+            req.logger.info("No local audio file to stream, redirecting to remote audio source.")
+            req.logger.debug("Redirecting to :\(remoteURL.absoluteString)")
+            return req.redirect(to: remoteURL.absoluteString, redirectType: .temporary)
+        }
+
         guard let localURL = audio.localURL
-        else { throw Errors.fileNotDownloaded }
+        else { throw Errors.fileNotAccessible }
 
         guard await req.downloadManager.tasks[id] == nil
         else { throw Errors.filePartiallyDownloaded }
@@ -33,7 +40,8 @@ extension EpisodeAudioController {
     enum Errors {
         static var unknownID: Abort { PodcastsController.Errors.unknownID }
         static var invalidIDFormat: Abort { PodcastsController.Errors.invalidIDFormat }
-        static var fileNotDownloaded: Abort { Abort(.unprocessableEntity, reason: "Audio file has not been downloaded. Unable to stream.") }
+        static var fileNotAccessible: Abort { Abort(.unprocessableEntity, reason: "No way to access audio file has been granted. Unable to stream.") }
+        //static var fileNotDownloaded: Abort { Abort(.unprocessableEntity, reason: "Audio file has not been downloaded. Unable to stream.") }
         static var filePartiallyDownloaded: Abort { Abort(.unprocessableEntity, reason: "Audio file is partially downloaded. Unable to stream.") }
     }
 }
