9cf4ae6e4149095af57e7f6f5ff3809484f933ca
Lukas Simonson <lukas@simonson.co>
2026-01-23 11:41:57 -0700

    Cleans Up Configuration

--- Sources/Banshee-Server/Utils/FileUtils.swift
+++ Sources/Banshee-Server/Utils/FileUtils.swift
@@ -1,3 +1,4 @@
+import Foundation
 
 enum FileUtils {
 
@@ -54,5 +55,13 @@ enum FileUtils {
         }
 
         return sanitize("\(season)\(epNumber) - \(episode.title)")
+    }
+}
+
+extension FileManager {
+    func pathExists(_ path: String) -> (exists: Bool, isDirectory: Bool) {
+        var isDirectory = ObjCBool(false)
+        let exists = self.fileExists(atPath: path, isDirectory: &isDirectory)
+        return (exists, isDirectory.boolValue)
     }
 }
--- Sources/Banshee-Server/configure.swift
+++ Sources/Banshee-Server/configure.swift
@@ -11,86 +11,103 @@ import QueuesFluentDriver
 //  - STORAGE_PATH: Where downloaded files should be stored.
 //  - METADATA_PATH: Where metadata files should be stored.
 
-// configures your application
+// Debug Environment Recommendations
+//  These paths will be ignored by git.
+//  - STORAGE_PATH: ./.AppData/Storage
+//  - METADATA_PATH: ./.AppData/Metadata
+
 public func configure(_ app: Application) async throws {
     // uncomment to serve files from /Public folder
     // app.middleware.use(FileMiddleware(publicDirectory: app.directory.publicDirectory))
-    try await configureXML(app)
-    try await configureAuth(app)
-    try await configureDownloads(app)
-    try await configureDatabase(app)
-    try await configureJobs(app)
+    
+    // Base Configuration
+    try await Configure(app: app)
 
     // register routes
     try routes(app)
 }
 
-private func configureXML(_ app: Application) async throws {
-    let xmlDecoder = XMLDecoder.rssDecoder()
-
-    xmlDecoder.shouldProcessNamespaces = true
-    xmlDecoder.namespaceFilteringStrategy = .stripByPrefix(["itunes"])
+struct Configure {
+    let app: Application
     
-    ContentConfiguration.global.use(decoder: xmlDecoder, for: .xml)
-    ContentConfiguration.global.use(decoder: xmlDecoder, for: .init(type: "application", subType: "rss+xml"))
-    ContentConfiguration.global.use(decoder: xmlDecoder, for: .init(type: "text", subType: "xml"))
-}
-
-private func configureAuth(_ app: Application) async throws {
-    await app.jwt.keys.add(hmac: HMACKey(stringLiteral: try getEnvironmentValue("JWT_SECRET")), digestAlgorithm: .sha256)
-}
-
-private func configureDownloads(_ app: Application) async throws {
-    let path = try getEnvironmentValue("STORAGE_PATH")
-    app.downloadManager = DownloadManager(storageBasePath: path, logger: app.logger)
-}
-
-private func configureDatabase(_ app: Application) async throws {
-//    let configuration = DatabaseConfigurationFactory.postgres(configuration: SQLPostgresConfiguration(
-//        hostname: try getEnvironmentValue("DATABASE_HOST"),
-//        port: Int(try getEnvironmentValue("DATABASE_PORT")) ?? SQLPostgresConfiguration.ianaPortNumber,
-//        username: try getEnvironmentValue("DATABASE_USERNAME"),
-//        password: try getEnvironmentValue("DATABASE_PASSWORD"),
-//        database: try getEnvironmentValue("DATABASE_NAME"),
-//        tls: .prefer(try .init(configuration: .clientDefault)) 
-//    ))
-//
-//    app.databases.use(configuration, as: .psql)
-
-    let metadata = try getEnvironmentValue("METADATA_PATH")
-    app.databases.use(.sqlite(.file("\(metadata)/banshee.sqlite")), as: .sqlite)
-
-    // Migrations
-    app.migrations.add(User.Migration.Create())
+    @discardableResult
+    init(app: Application) async throws {
+        self.app = app
+        
+        try await xml()
+        try await auth()
+        try await downloads()
+        try await database()
+        try await jobs()
+    }
     
-    app.migrations.add(Podcast.Migration.Create())
-    app.migrations.add(PodcastConfig.Migration.Create())
-    app.migrations.add(RSSConfig.Migration.Create())
+    private func xml() async throws {
+        let xmlDecoder = XMLDecoder.rssDecoder()
+
+        xmlDecoder.shouldProcessNamespaces = true
+        xmlDecoder.namespaceFilteringStrategy = .stripByPrefix(["itunes"])
+        
+        ContentConfiguration.global.use(decoder: xmlDecoder, for: .xml)
+        ContentConfiguration.global.use(decoder: xmlDecoder, for: .init(type: "application", subType: "rss+xml"))
+        ContentConfiguration.global.use(decoder: xmlDecoder, for: .init(type: "text", subType: "xml"))
+    }
     
-    app.migrations.add(Episode.Migration.Create())
-    app.migrations.add(EpisodeConfig.Migration.Create())
-    app.migrations.add(AudioConfig.Migration.Create())
+    private func auth() async throws {
+        await app.jwt.keys.add(hmac: HMACKey(stringLiteral: try environmentValue("JWT_SECRET")), digestAlgorithm: .sha256)
+    }
+    
+    private func downloads() async throws {
+        let path = try environmentValue("STORAGE_PATH")
+        app.downloadManager = DownloadManager(storageBasePath: path, logger: app.logger)
+    }
+    
+    private func database() async throws {
+        let metadata = try environmentValue("METADATA_PATH")
+        
+        // Create the directory if it doesn't exist
+        let (doesExist, isDirectory) = FileManager.default.pathExists(metadata)
+        if !doesExist {
+            try FileManager.default.createDirectory(atPath: metadata, withIntermediateDirectories: true)
+        } else if !isDirectory {
+            throw ConfigError.metadataPathNotADirectory
+        }
+        
+        app.databases.use(.sqlite(.file("\(metadata)/banshee.sqlite")), as: .sqlite)
 
-    try await app.autoMigrate()
-}
+        // Migrations
+        app.migrations.add(User.Migration.Create())
+        
+        app.migrations.add(Podcast.Migration.Create())
+        app.migrations.add(PodcastConfig.Migration.Create())
+        app.migrations.add(RSSConfig.Migration.Create())
+        
+        app.migrations.add(Episode.Migration.Create())
+        app.migrations.add(EpisodeConfig.Migration.Create())
+        app.migrations.add(AudioConfig.Migration.Create())
 
-private func configureJobs(_ app: Application) async throws {
-    // Jobs Migration
-    app.migrations.add(JobModelMigration())
+        try await app.autoMigrate()
+    }
+    
+    private func jobs() async throws {
+        app.migrations.add(JobModelMigration())
 
-    app.queues.use(.fluent(.sqlite))
+        app.queues.use(.fluent(.sqlite))
 
-    app.queues.schedule(RSSFeedJob())
-        .hourly()
-        .at(0)
-}
-
-private func getEnvironmentValue(_ key: String) throws -> String {
-    guard let value = Environment.get(key)
-    else { throw ConfigurationError.missingEnvironmentValue(key) }
-    return value
-}
-
-private enum ConfigurationError: Error {
-    case missingEnvironmentValue(String)
+        app.queues.schedule(RSSFeedJob())
+            .hourly()
+            .at(0)
+        
+        try app.queues.startScheduledJobs()
+    }
+    
+    private func environmentValue(_ key: String) throws -> String {
+        guard let value = Environment.get(key)
+        else { throw ConfigError.missingEnvironmentValue(key) }
+        return value
+    }
+    
+    private enum ConfigError: Error {
+        case metadataPathNotADirectory
+        case missingEnvironmentValue(String)
+    }
 }
