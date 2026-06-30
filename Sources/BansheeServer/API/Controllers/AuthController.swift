7ffd6825af5f6d0039c101daa0a728086ef64973
Lukas Simonson <lukas@simonson.co>
2026-06-30 11:56:27 -0600

    fix(auth): add returning JWT on registration

--- Sources/BansheeServer/API/Controllers/AuthController.swift
+++ Sources/BansheeServer/API/Controllers/AuthController.swift
@@ -38,15 +38,16 @@ struct AuthController: RouteCollection {
         guard try await req.userDAO.adminCount() == 0
         else { throw Abort(.badRequest, reason: "An admin account already exists, please use the /api/auth/register endpoint to create a new user.") }
         
-        return try await req.userDAO
-            .create(
-                email: registerRequest.email,
-                username: registerRequest.username,
-                name: registerRequest.name,
-                passwordHash: req.password.async.hash(registerRequest.password),
-                role: registerRequest.role
-            )
-            .toDTO()
+        let user = try await req.userDAO.create(
+            email: registerRequest.email,
+            username: registerRequest.username,
+            name: registerRequest.name,
+            passwordHash: req.password.async.hash(registerRequest.password),
+            role: registerRequest.role
+        )
+        
+        return try await user
+            .toDTO(with: req.jwt.sign(user.token()))
             .encodeResponse(status: .created, for: req)
     }
     
@@ -59,15 +60,16 @@ struct AuthController: RouteCollection {
         try RegisterRequest.validate(content: req)
         let registerRequest = try req.content.decode(RegisterRequest.self)
         
-        return try await req.userDAO
-            .create(
-                email: registerRequest.email,
-                username: registerRequest.username,
-                name: registerRequest.name,
-                passwordHash: req.password.async.hash(registerRequest.password),
-                role: registerRequest.role
-            )
-            .toDTO()
+        let user = try await req.userDAO.create(
+            email: registerRequest.email,
+            username: registerRequest.username,
+            name: registerRequest.name,
+            passwordHash: req.password.async.hash(registerRequest.password),
+            role: registerRequest.role
+        )
+        
+        return try await user
+            .toDTO(with: req.jwt.sign(user.token()))
             .encodeResponse(status: .created, for: req)
     }
     
